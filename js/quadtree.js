af361cb08ee7d14883c657a992891ebb8ac7567b
jackuss1981 <jackuss@gmail.com>
2019-08-21 15:34:53 +0200

    Added quadtree from example, made by Daniel Shiffman

--- js/quadtree.js
+++ js/quadtree.js
@@ -1,14 +1,101 @@
 /*
 	Javascript for creating quadtrees. A way to check for collisions 
-	Hopefully it will also help with better memory management
+	Hopefully it will  help with better memory management
 
 */
+class QuadTree {
+  constructor(boundary, n) {
+    this.boundary = boundary;
+    this.capacity = n;
+    this.numbers = [];
+    this.divided = false;
+  }
 
-class quadTree {
-	constructor() {
-		
-		
-	}
-	
-	
+  subdivide() {
+    let x = this.boundary.x;
+    let y = this.boundary.y;
+    let w = this.boundary.w;
+    let h = this.boundary.h;
+    let ne = new Rectangle(x + w / 2, y - h / 2, w / 2, h / 2);
+    this.northeast = new QuadTree(ne, this.capacity);
+    let nw = new Rectangle(x - w / 2, y - h / 2, w / 2, h / 2);
+    this.northwest = new QuadTree(nw, this.capacity);
+    let se = new Rectangle(x + w / 2, y + h / 2, w / 2, h / 2);
+    this.southeast = new QuadTree(se, this.capacity);
+    let sw = new Rectangle(x - w / 2, y + h / 2, w / 2, h / 2);
+    this.southwest = new QuadTree(sw, this.capacity);
+    this.divided = true;
+  }
+
+  insert(number) {
+
+    if (!this.boundary.contains(number)) {
+      return false;
+    }
+
+    if (this.numbers.length < this.capacity) {
+      this.numbers.push(number);
+      return true;
+    } else {
+      if (!this.divided) {
+        this.subdivide();
+      }
+      if (this.northeast.insert(number)) {
+        return true;
+      } else if (this.northwest.insert(number)) {
+        return true;
+      } else if (this.southeast.insert(number)) {
+        return true;
+      } else if (this.southwest.insert(number)) {
+        return true;
+      }
+    }
+  }
+
+  query(range, found) {
+    if (!found) {
+      found = [];
+    }
+    if (!this.boundary.intersects(range)) {
+      return;
+    } else {
+      for (let n of this.numbers) {
+        if (range.contains(n)) {
+          found.push(n);
+        }
+      }
+      if (this.divided) {
+        this.northwest.query(range, found);
+        this.northeast.query(range, found);
+        this.southwest.query(range, found);
+        this.southeast.query(range, found);
+      }
+    }
+    return found;
+  }
+
+
+  show() {
+    stroke(255);
+    noFill();
+    strokeWeight(1);
+    rectMode(CENTER);
+    rect(this.boundary.x, this.boundary.y, this.boundary.w * 2, this.boundary.h * 2);
+    for (let p of this.numbers) {
+      strokeWeight(2);
+      number(p.x, p.y);
+    }
+
+    if (this.divided) {
+      this.northeast.show();
+      this.northwest.show();
+      this.southeast.show();
+      this.southwest.show();
+    }
+  }
+
+
+
+
+
 }
